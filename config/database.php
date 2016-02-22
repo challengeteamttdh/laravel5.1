c788e25e862e65a36c637eacd9a63fc1197f7ca8
phanha <phanthanhhak55hus@gmail.com>
2016-02-22 11:22:53 +0700

    add shop temp

--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'laravel5-bootstrap'),
+            'database'  => env('DB_DATABASE', 'laravel5'),
             'username'  => env('DB_USERNAME', 'root'),
             'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
