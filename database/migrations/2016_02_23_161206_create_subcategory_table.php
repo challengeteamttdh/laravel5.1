a6b60afd6442dbed84996b9690b3109fdadd0943
challengeteamttdh <challengeteamttdh@gmail.com>
2016-02-23 23:24:05 +0700

    updated migration database

---
+++ database/migrations/2016_02_23_161206_create_subcategory_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubcategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subcategory', function (Blueprint $table) {
+            $table->increments('subcategory_id');
+            $table->integer('subcategory_category_id')->unsigned();
+            $table->foreign('subcategory_category_id')->references('category_id') ->on('category');
+            $table->text('subcategory_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('subcategory');
+    }
+}
