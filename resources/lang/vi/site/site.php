c90c6da5af6d99d126778a586687da193f18fad7
challengeteamttdh <challengeteamttdh@gmail.com>
2016-02-24 21:31:47 +0700

    Added file

---
+++ resources/lang/vi/site/site.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+	'home' => 'Trang Chu',
+	'login' => 'Dang Nhap',
+	'sign_up' => 'Dang Ky',
+	'admin_panel' => 'Trang Quan Tri',
+	'logout' => 'Dang Xuat',
+	'login_as' => 'Dang nhap nhu',
+	
+
+];
