3735e18893704e27fa5cd4a7ce857ab4f82ac175
challengeteamttdh <challengeteamttdh@gmail.com>
2016-03-09 00:41:03 +0700

    update footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,21 +1,19 @@
 <footer id="footer"><!--Footer-->
     <div class="footer-widget">
             <div class="footer_ct">
-                <p><a href="http://noithatchangson.vn/"><span style="font-size:16px"><strong>Cơ Sở Sản Xuất và&nbsp;Tư
-                                Vấn Thiết Kế&nbsp;Nội Thất Chàng Sơn</strong></span></a></p>
-                <p><a href="http://noithatchangson.vn/">http://noithatchangson.vn</a></p>
-                <p>Địa chỉ xưởng&nbsp;: Số 9 KCN&nbsp;Chàng Sơn -&nbsp;Thạch Thất - Hà Nội - Việt Nam</p>
-                <p>VPĐD: P807 - CT1A - Tòa C14 Bộ Công An - Lê Văn Lương kéo dài - Nam Từ Liêm - Hà Nội</p>
-                <p>SĐT: 0972681291</p>
-                <p>Email: noithatchangson.vn@gmail.com</p>
-                <p>Bản quyền thuộc về Cơ Sở Sản Xuất Và&nbsp;Tư Vấn Thiết Kế Nội Thất Chàng Sơn</p>
+                <p><a href="http://noithatchangson.vn/"><span style="font-size:16px"><strong>NỘI THẤT NGỌC DŨNG</strong></span></a></p>
+                <p><a href="http://noithatngocdung.vn/">http://noithatngocdung.vn</a></p>
+                <p>Địa chỉ xưởng&nbsp;: Dị Nậu -&nbsp;Thạch Thất - Hà Nội - Việt Nam</p>
+                <p>SĐT: 0983438810</p>
+                <p>Email: noithatngocdung.vn@gmail.com</p>
+                <p>Bản quyền thuộc về Cơ Sở Sản Xuất Và&nbsp;Tư Vấn Thiết Kế Nội Thất Ngọc Dũng</p>
             </div>
     </div>
 
     <div class="footer-bottom">
         <div class="container">
             <div class="row">
-                <p class="pull-left">Copyright © 2015 Noi that ngoc dung. All rights reserved.</p>
+                <p class="pull-left">Copyright © 2016 Noi that ngoc dung. All rights reserved.</p>
             </div>
         </div>
     </div>
