3b6622fd1051d7b23eafa94282c938dc2305a77a
phanha <phanthanhhak55hus@gmail.com>
2016-03-11 09:20:59 +0700

    conflict

--- unzip.php
+++ unzip.php
@@ -1,20 +1,13 @@
 <?php
 $zip = new ZipArchive;
-<<<<<<< HEAD
+
 define('DIR_FOLDER', $_SERVER['DOCUMENT_ROOT']);
 $res = $zip->open('vendor.zip');
-=======
-$res = $zip->open('laravel5.1.zip');
-define('DIR_FOLDER', $_SERVER['DOCUMENT_ROOT']);
->>>>>>> 030bff704593dd0a1f0d4af5841227d61a536e3e
+
 if ($res === TRUE) {
   $zip->extractTo(DIR_FOLDER);
   $zip->close();
   echo 'woot!';
 } else {
-<<<<<<< HEAD
   echo 'doh! , cannot open file';
-=======
-  echo 'doh!';
->>>>>>> 030bff704593dd0a1f0d4af5841227d61a536e3e
 }
